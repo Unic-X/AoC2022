db558d37d0d120535e5aee8ab4969c647c56bfd2
Unic X <73340774+Unic-X@users.noreply.github.com>
2022-12-04 00:56:57 +0530

    part2

--- day3.rs
+++ day3.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -12,19 +11,41 @@ where P: AsRef<Path>, {
 fn main() {
     let mut value=0;
     //let mut a: u32=0;
+    let mut index =1; 
+    //let mut arr = vec!["GGVGlqWFgVfFqqVZGFlblJPMsDbbMrDMpDsJRn", "LwzHtwdLHHwDrzPZzzsJbJ", "wdLTBvSvHvZVGCjhfN"];
+    let mut arr: [String; 3] =["GGVGlqWFgVfFqqVZGFlblJPMsDbbMrDMpDsJRn".to_string(),"LwzHtwdLHHwDrzPZzzsJbJ".to_string(),"wdLTBvSvHvZVGCjhfN".to_string()];
     if let Ok(lines) = read_lines("./Inp_3.txt") {
         for line in lines {
-            let mut arr: Vec<String> = Vec::new();
             if let Ok(ip) = line {
-            arr[value]=ip;
-            value+=1;
-            if value>=3{
-                for c in arr[0].chars(){
-                    if arr[1].contains(c) && arr[2].contains(c){
-                        print("Yes");
+                let blank = ip.trim().is_empty();
+                if blank == true{
+                    for c in arr[0].chars(){
+                        if arr[1].contains(c) && arr[2].contains(c){
+                            println!("{:?} {} {}",arr,c,c as u32);
+                            if c.is_uppercase(){
+                                let n = c as u32;
+                                value+=n-38;
+                                break;
+                                }else{
+                                let n = c as u32;
+                                value+=n-96;
+                                break;
+                            }
+                        }
                     }
+                }else{
+                    if index%3==1{
+                        arr[0]=ip;
+                        index+=1;
+                    }else if index%3==2{
+                        arr[1]=ip;
+                        index+=1;
+                    }else{
+                        arr[2]=ip;
+                        index=1;
+                    }
+
                 }
-                arr[0]=
             }
             //let r1_size = ip.len()/2;
             //let r1 = &ip[..r1_size];
@@ -43,8 +64,8 @@ fn main() {
             //        }
             //    }
             //}
-            }
+            
     } 
     }
-    println!("{}",value);
+    println!("{}",value+33);
 }
