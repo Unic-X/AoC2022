eb4db8b96a96fa9ff3f6968c8452b0ea98ccf466
Unic X <73340774+Unic-X@users.noreply.github.com>
2022-12-07 11:24:28 +0530

    part 1

--- day5.rs
+++ day5.rs
@@ -4,18 +4,24 @@ use std::path::Path;
 extern crate regex;
 use regex::Regex;
 
-
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
+fn remove_first<T>(vec: &mut Vec<T>) -> Option<T> {
+    if vec.is_empty() {
+        return None;
+    }
+    Some(vec.remove(0))
+}
+
 fn main(){
     //let mut vec: Vec<Vec<char>> = Vec::with_capacity(9 * 8);
 
-    let mut vec = vec![vec!['#'; 8]; 9];
+    let mut vec = vec![Vec::with_capacity(8);9];
+    //let mut r_vec = vec![vec![char]];
     if let Ok(lines) = read_lines("./Inp_5.txt") {
         let mut l_i = 0;
         let mut t_m = 0;                                //no of items to move
@@ -28,22 +34,24 @@ fn main(){
                     for i in (1..ip.len()).step_by(4){
                         let emp = ip.chars().nth(i).unwrap().is_whitespace();
                         if emp == false{
-                            vec[lis_n][l_i] = ip.chars().nth(i).unwrap();
+                            vec[lis_n].push(ip.chars().nth(i).unwrap());
                         }
-                        lis_n+=1
+                        lis_n+=1;
                     };
                 }else if l_i>9{
-                    let re = Regex::new(r"move ([\s \S]*) from ([\s \S]*) to ([\s \S]*)").unwrap();
-                    let caps = re.captures(&ip).unwrap();
-                    t_m = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
-                    c_n = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
-                    t_n = caps.get(3).unwrap().as_str().parse::<u32>().unwrap();
-                    for t in (0..t_m).rev(){
-                        println!("move {:?} from {} to {}",vec[(c_n-1) as usize],c_n,t_n);
-                        let new = vec[(c_n-1) as usize].pop().unwrap();
-                        vec[(t_n-1) as usize].push(new);
-                }
-                }
+                   let re = Regex::new(r"move ([\s \S]*) from ([\s \S]*) to ([\s \S]*)").unwrap();
+                   let caps = re.captures(&ip).unwrap();
+                   t_m = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
+                   c_n = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
+                   t_n = caps.get(3).unwrap().as_str().parse::<u32>().unwrap();
+                   for i in 0..t_m{
+                    let item = vec[(c_n-1) as usize][0];
+                    println!("{}",item);
+                    vec[(c_n-1) as usize].remove(0);
+                    vec[(t_n-1) as usize].insert(0,item);
+                    
+                   }
+                   //let item = vec[c_n]
             }
             l_i+=1;
         }
@@ -51,4 +59,6 @@ fn main(){
     vec.iter().for_each(|it| {
         println!("{:?}", it);})
 }
+}
+
 
